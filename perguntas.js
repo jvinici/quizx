ac5b64d6c999180b55c9f62ef455962a3108821f
jvinici <85917550+jvinici@users.noreply.github.com>
2024-11-14 13:59:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História:',
+    'Quando começou e terminou a Guerra Fria?',
+    'Começou em 1947, e terminou em 1991'
+)
+
+criaCartao(
+    'Geografia:',
+    'Qual o tamanho da Av.Paulista?',
+    'A Av.Paulista tem 2,7 km de extensão'
+)
+
+criaCartao(
+    'Biologia:',
+    'Quantos ossos tem o corpo humano?',
+    'O corpo humano tem 206 ossos.'
+)
+
+criaCartao(
+    'Matemática:',
+    'Quando uma figura geometica tem 4 lados iguais, como ele se chama?',
+    'Ele se chamará, "quadrado".'
+)
